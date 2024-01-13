2a957485ca08aff3d036f48f1d89866083612189
Mikołaj Maciejak <myster123987@gmail.com>
2024-01-13 13:15:22 +0100

    fixed dark mode once more

--- src/lib/switch-dark-mode.js
+++ src/lib/switch-dark-mode.js
@@ -2,17 +2,16 @@ export const switchDarkMode = () => {
   const currentMode = document.querySelector("html");
   const iconOfDarkMode = document.querySelector("header > button > img");
   const arrowLeft = document.getElementById("arrowLeft");
-  const bodyBackgroudnd = document.querySelector("body");
 
   if (currentMode.classList.contains("dark")) {
     currentMode.classList.remove("dark");
     iconOfDarkMode.classList.remove("white-svg-fill");
     arrowLeft.classList.remove("white-svg-fill");
-    bodyBackgroudnd.style.backgroundColor = "hsl(0, 0%, 95%)";
+    document.body.style.backgroundColor = "hsl(0, 0%, 95%)";
   } else {
     currentMode.classList.add("dark");
     iconOfDarkMode.classList.add("white-svg-fill");
     arrowLeft.classList.add("white-svg-fill");
-    bodyBackgroudnd.style.backgroundColor = "#27363f";
+    document.body.style.backgroundColor = "#27363f";
   }
 };
